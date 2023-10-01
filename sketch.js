9e84761fb50f849120bcf9ed467b78d63231ab05
JACK SEAMANS <JJS22F@FSU.EDU>
2023-10-01 16:58:08 -0400

    work and school buttons on pic

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-let button;
+// let button;
 
-function setup() {
-    createCanvas(800, 550);
-    button = createButton('School');
-    button.position(300, 500)
-    button.mousePressed(goToSchool)
-}
+// function setup() {
+//     createCanvas(800, 550);
+//     button = createButton('School');
+//     button.position(300, 500)
+//     button.mousePressed(goToSchool)
+// }
 
