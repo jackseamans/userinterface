40cf3d5073b17c4530fc1bf08c0ad2cefc301b66
JACK SEAMANS <JJS22F@FSU.EDU>
2023-10-09 11:26:18 -0400

    Added p5 (whoops) and added more choices

--- sketch.js
+++ sketch.js
@@ -4,8 +4,29 @@ schoolRoutesButton.addEventListener("click", schoolRoutes);
 workRoutesButton.addEventListener("click", workRoutes);
 
 function setup() {
-  noCanvas();
+  createCanvas(120,50);
+  radio = createRadio();
+  radio.option('1', 'apple');
+  radio.option('2', 'bread');
+  radio.option('3', 'juice');
+  radio.style('width', '30px');
+  radio.selected('2');
+  textAlign(CENTER);
 }
+
+function draw() {
+  background(200);
+  let val = radio.value();
+  if (val) {
+    text('item cost is $' + val, width / 2, height / 2);
+  }
+}
+
+// function setup() {
+//   createCanvas(600, 600);
+//   background(0);
+//   workRoutesButton.position(600, 600);
+// }
 
 function schoolRoutes() {
   console.log("Button clicked!");
